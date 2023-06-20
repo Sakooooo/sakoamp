fea860fea0aedfa42db8ede493d9338f6eeb6e31
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-06-20 21:53:51 +0400

    server select setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  // const [greetMsg, setGreetMsg] = useState("");
+  // const [name, setName] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
-  return <div className="App"></div>;
+  async function checkFile(file) {}
+
+  return (
+    <div className="App">
+      <div className="ServerPicker">
+        <form>
+          <label for="server_address">Server Address</label>
+          <input type="text" id="server_address"></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
