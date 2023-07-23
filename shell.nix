9abeb0d62b8c3850cf52c2c51e708d3d2f6a5056
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-23 22:37:12 +0300

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+
+mkShell {
+  name = "SakoAmp- Nix Shell";
+  nativeBuildInputs = [
+    nodejs
+    rust
+    yarn
+    rustup
+    cargo
+  ];
+  buildInputs = with python310Packages; [
+    rust-tauri
+  ];
+
+}
